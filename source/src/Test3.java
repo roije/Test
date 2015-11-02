17083e906841aa20fa3f29a81432646e60c3b1a3
christianhasselstrom <crellecrelle@gmail.com>
2015-11-02 13:03:59 +0100

    Ny hello world

---
+++ source/src/Test3.java
@@ -0,0 +1,11 @@
+/**
+ * Created by christianhasselstrom on 02/11/2015.
+ */
+public class Test3 {
+
+    public static void main(String[] args)
+
+    {
+        System.out.println("Hvad");
+    }
+}
